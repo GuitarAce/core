445961a1eaa41299c5e1bc06d36b84feacf7d0b8
Guitar <53537078+GuitarAce@users.noreply.github.com>
2021-02-04 13:54:30 +0700

    enter

--- apps/models.py
+++ apps/models.py
@@ -6,6 +6,7 @@ class ClassProject(models.Model):
     nameproject = models.CharField(max_length=50)
     listname = models.CharField(max_length = 50)
     professorname = models.CharField(max_length=50)
+    detailproject = models.TextField(default = "Input Detail Project")
 
     def __str__(self):
         return self.nameproject
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import ClassProject
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def createclasspj(request):
+    return createclasspj
