b15167245c4397fc4c41cc0f2f4458fb4086180c
Guitar <53537078+GuitarAce@users.noreply.github.com>
2021-02-16 10:42:26 +0700

    enter

--- apps/models.py
+++ apps/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
-class ProjectTopic(models.Model):
-    nameprojectTopic = models.CharField(db_column='nameprojectTopic',max_length=50)
-    listproject = models.CharField(db_column='listproject',max_length=50)
-    professorname = models.CharField(db_column='professorname',max_length=50)
-    year = models.CharField(db_column='year',max_length=50)
-    def __str__(self):
-        return self.nameprojectTopic
-        
+# Create your models here.        
 class Project(models.Model):
     name_project = models.CharField(db_column='name_project',max_length=50)
     list_name = models.CharField(db_column='list_name',max_length = 50)
@@ -18,5 +9,18 @@ class Project(models.Model):
     def __str__(self):
         return self.name_project
 
+class ProjectTopic(models.Model):
+    nameprojectTopic = models.CharField(db_column='nameprojectTopic',max_length=50)
+    # listproject = models.ForeignKey(Project, on_delete=models.CASCADE)
+    professorname = models.CharField(db_column='professorname',max_length=50)
+    def __str__(self):
+        return self.nameprojectTopic
 
+class ProjectExam(models.Model):
+    nameExam = models.CharField(db_column='nameExam',max_length=50)
+    # listnames = models.ForeignKey(Project, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nameExam
+    class Meta:
+        ordering = ['nameExam']
 
