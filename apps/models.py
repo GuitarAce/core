551374c0c0258be48b71c8ded63245824c2e79a3
Guitar <53537078+GuitarAce@users.noreply.github.com>
2021-02-16 07:46:07 +0700

    enter

--- apps/models.py
+++ apps/models.py
@@ -3,19 +3,20 @@ from django.db import models
 # Create your models here.
 
 class ProjectTopic(models.Model):
-    nameproject = models.CharField(max_length=50)
-    listproject = models.CharField(max_length = 50)
-    professorname = models.CharField(max_length=50)
-    year = models.CharField(max_length=50)
-
+    nameprojectTopic = models.CharField(db_column='nameprojectTopic',max_length=50)
+    listproject = models.CharField(db_column='listproject',max_length=50)
+    professorname = models.CharField(db_column='professorname',max_length=50)
+    year = models.CharField(db_column='year',max_length=50)
     def __str__(self):
-        return self.nameproject
-
+        return self.nameprojectTopic
+        
 class Project(models.Model):
     name_project = models.CharField(db_column='name_project',max_length=50)
     list_name = models.CharField(db_column='list_name',max_length = 50)
     professor_name = models.CharField(db_column='professor_name',max_length=50)
     detail_project = models.TextField(db_column='detail_project',max_length=3000)
-
     def __str__(self):
         return self.name_project
+
+
+
