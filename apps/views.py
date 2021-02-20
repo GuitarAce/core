aaefea6050ef5b81508688955ae118aa8c993e89
Guitar <53537078+GuitarAce@users.noreply.github.com>
2021-02-21 00:11:22 +0700

    enter

--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
 from .models import ProjectTopic, Project
 from .forms import ProjectTopicssForm, ProjectForm
 # Create your views here.
