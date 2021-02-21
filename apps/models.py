a1fed4ce281333c68cede938c3549c50f7a17c48
Guitar <53537078+GuitarAce@users.noreply.github.com>
2021-02-22 06:13:04 +0700

    e

--- apps/models.py
+++ apps/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     name_project = models.CharField(db_column='name_project',max_length=50)
     list_name = models.CharField(db_column='list_name',max_length = 50)
     professor_name = models.CharField(db_column='professor_name',max_length=50)
-    detail_project = models.TextField(db_column='detail_project',max_length=3000)
+    detail_project = models.FileField(upload_to='uploads/')
     def __str__(self):
         return self.name_project
 
